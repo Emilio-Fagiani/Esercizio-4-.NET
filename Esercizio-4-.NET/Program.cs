c569c6cbd7c0a6ecc009f9c31ec75ce06ff375de
emifa <emifa@LAPTOP-1TM449QB>
2023-06-21 13:10:29 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-4-.NET/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please, insert text");
+string text = Console.ReadLine();
+char[] txet = text.ToCharArray();
+for (int i = txet.Length-1;i >= 0; i--)
+{
+    Console.Write(txet[i]);
+}
+Console.WriteLine();    
